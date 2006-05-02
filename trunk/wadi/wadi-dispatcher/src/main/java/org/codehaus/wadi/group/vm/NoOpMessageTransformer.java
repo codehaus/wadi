d8fbd0ce0fa3b4e8c5417afa60f14be4d8fdb7c9
gdamour <gdamour@850eb8eb-920f-0410-90bf-d29e371206e5>
2006-05-02 14:26:38 +0000

    By default, messages exchanged via the in-vm dispatcher are serialized.

--- trunk/wadi/wadi-dispatcher/src/main/java/org/codehaus/wadi/group/vm/NoOpMessageTransformer.java
+++ trunk/wadi/wadi-dispatcher/src/main/java/org/codehaus/wadi/group/vm/NoOpMessageTransformer.java
@@ -15,34 +15,15 @@
  */
 package org.codehaus.wadi.group.vm;
 
-import java.io.Serializable;
-import java.util.Map;
-import org.codehaus.wadi.group.LocalPeer;
-import org.codehaus.wadi.group.MessageExchangeException;
-
+import org.codehaus.wadi.group.Message;
 
 /**
  * 
  * @version $Revision: 1603 $
  */
-public class VMLocalPeer extends VMPeer implements LocalPeer, Serializable {
+public class NoOpMessageTransformer implements MessageTransformer {
 
-    public VMLocalPeer(String name) {
-        super(name);
-    }
-    
-    public void setState(Map newState) throws MessageExchangeException {
-        synchronized (state) {
-            state.clear();
-            state.putAll(newState);
-        }
-    }
-    
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-    
-    public String toString() {
-        return "Local Node :" + name;
+    public Message transform(Message message) {
+        return message;
     }
 }
