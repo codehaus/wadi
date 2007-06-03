6e6952406538709ecb5d0200e756014592e4bb5d
gdamour <gdamour@850eb8eb-920f-0410-90bf-d29e371206e5>
2007-06-03 05:30:21 +0000

    remove unecessary config arg parameter

--- trunk/wadi/wadi-tribes/src/test/java/org/codehaus/wadi/tribes/TestTribesRelocation.java
+++ trunk/wadi/wadi-tribes/src/test/java/org/codehaus/wadi/tribes/TestTribesRelocation.java
@@ -6,10 +6,9 @@ import org.codehaus.wadi.relocation.AbstractTestRelocation;
 
 public class TestTribesRelocation extends AbstractTestRelocation {
     private String clusterName = "dummy";
-    private long timeout = 5000;
 
     protected Dispatcher newDispatcher(String name) {
-        return new TribesDispatcher(clusterName, name, null, "");
+        return new TribesDispatcher(clusterName, name, null);
     }
     
 }
