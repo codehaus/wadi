0b84b8a606a6afb748df6f9086da6bbdbc49bc9b
gdamour <gdamour@850eb8eb-920f-0410-90bf-d29e371206e5>
2008-02-26 13:09:58 +0000

    No need to apply a filter on action.

--- trunk/wadi/wadi-console/grails-app/conf/ClusterSelectedFilters.groovy
+++ trunk/wadi/wadi-console/grails-app/conf/ClusterSelectedFilters.groovy
@@ -6,7 +6,7 @@ class ClusterSelectedFilters {
             before = {
                 def controllerName = request.getAttribute(GAA.CONTROLLER_NAME_ATTRIBUTE)
                 def actionName= request.getAttribute(GAA.ACTION_NAME_ATTRIBUTE)
-                if ('selectCluster' == controllerName && 'list' == actionName) {
+                if ('selectCluster' == controllerName) {
                     return true
                 }
                 if (null == session[SelectClusterController.SESSION_KEY_SELECTED_CLUSTER_NAME]) {
