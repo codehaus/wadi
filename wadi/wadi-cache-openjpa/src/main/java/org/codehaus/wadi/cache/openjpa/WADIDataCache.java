8c0fafe6938e88fa66e11655229d5329be5d1a4a
gdamour <gdamour@850eb8eb-920f-0410-90bf-d29e371206e5>
2009-02-23 10:29:17 +0000

    Look-up a Dispatcher instead of creating one. We can now loose the wadi-tribes dependency.
    Within Geronimo, using a StaticDispatcherRegistry allows the default Dispatcher to be retrieved.
    
    Also, extract methods to improve understanding.

--- wadi/wadi-cache-openjpa/src/main/java/org/codehaus/wadi/cache/openjpa/WADIDataCache.java
+++ wadi/wadi-cache-openjpa/src/main/java/org/codehaus/wadi/cache/openjpa/WADIDataCache.java
@@ -34,69 +34,66 @@ import org.codehaus.wadi.cache.basic.core.BasicGlobalObjectStore;
 import org.codehaus.wadi.cache.basic.core.BasicInTxCacheFactory;
 import org.codehaus.wadi.cache.basic.entry.AccessListener;
 import org.codehaus.wadi.cache.basic.entry.BasicAccessListener;
+import org.codehaus.wadi.cache.basic.entry.GlobalObjectStore;
 import org.codehaus.wadi.cache.policy.OptimisticAcquisitionPolicy;
 import org.codehaus.wadi.cache.policy.PessimisticAcquisitionPolicy;
 import org.codehaus.wadi.cache.policy.ReadOnlyAcquisitionPolicy;
 import org.codehaus.wadi.cache.util.TransactionManagerAwareCache;
 import org.codehaus.wadi.cache.util.TxDecoratorCache;
+import org.codehaus.wadi.core.WADIRuntimeException;
 import org.codehaus.wadi.core.manager.Manager;
 import org.codehaus.wadi.core.util.SimpleStreamer;
 import org.codehaus.wadi.core.util.Streamer;
 import org.codehaus.wadi.group.Dispatcher;
+import org.codehaus.wadi.group.DispatcherRegistry;
 import org.codehaus.wadi.group.LocalPeer;
 import org.codehaus.wadi.servicespace.ServiceSpace;
 import org.codehaus.wadi.servicespace.ServiceSpaceName;
-import org.codehaus.wadi.tribes.TribesDispatcher;
 
 /**
  *
  * @version $Rev:$ $Date:$
  */
 public class WADIDataCache extends AbstractDataCache {
-    private Cache cache;
-    private String nodeName;
+    protected String clusterName;
+    protected Class<DispatcherRegistry> dispatcherRegistryClass;
+    protected Cache cache;
 
-    public void setNodeName(String nodeName) {
-        this.nodeName = nodeName;
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+    
+    public void setDispatcherRegistry(String dispatcherRegistry) throws Exception {
+        ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+        this.dispatcherRegistryClass = (Class<DispatcherRegistry>) Class.forName(dispatcherRegistry, true, tccl);
     }
 
     @Override
     public void endConfiguration() {
         if (null == getName()) {
             throw new IllegalArgumentException("name property has not been set");
+        } else if (null == clusterName) {
+            throw new IllegalArgumentException("clusterName is required");
+        } else if (null == dispatcherRegistryClass) {
+            throw new IllegalArgumentException("dispatcherRegistryClass is required");
         }
-        // Add persistence unit name.
-        String cacheName = nodeName + "/" + getName();
         
-        try {
-            Dispatcher dispatcher = new TribesDispatcher("clusterName", nodeName, null);
-            dispatcher.start();
+        Dispatcher dispatcher = locateDispatcher();
 
-            ServiceSpaceName name = new ServiceSpaceName(URI.create(cacheName));
-            
-            CacheStackContext stackContext = new CacheStackContext(name, dispatcher);
-            stackContext.setDisableReplication(true);
-            stackContext.build();
+        try {
+            CacheStackContext stackContext = newCacheStackContext(dispatcher);
 
             ServiceSpace serviceSpace = stackContext.getServiceSpace();
             serviceSpace.start();
             
-            Manager manager = stackContext.getManager();
+            GlobalObjectStore globalObjectStore = newGlobalObjectStore(stackContext);
             
-            LocalPeer localPeer = serviceSpace.getLocalPeer();
-            AccessListener accessListener = new BasicAccessListener(localPeer);
-            Streamer streamer = new SimpleStreamer();
-            BasicGlobalObjectStore globalObjectStore = new BasicGlobalObjectStore(accessListener, manager, streamer);
-            
-            BasicInTxCacheFactory inTxCacheFactory = new BasicInTxCacheFactory(globalObjectStore);
-            cache = new BasicCache(globalObjectStore, inTxCacheFactory);
-            cache = new TxDecoratorCache(cache);
-            cache = new TransactionManagerAwareCache(cache, conf.getManagedRuntimeInstance().getTransactionManager());
+            cache = newCache(globalObjectStore);
         } catch (Exception e) {
-            System.out.println(e);
+            throw new WADIRuntimeException("Problem when creating Cache instance", e);
         }
     }
-    
+
     @Override
     public void commit(Collection additions, Collection newUpdates, Collection existingUpdates, Collection deletes) {
         super.commit(additions, newUpdates, Collections.EMPTY_LIST, deletes);
@@ -150,6 +147,54 @@ public class WADIDataCache extends AbstractDataCache {
         AcquisitionInfo pinAcquisition = new AcquisitionInfo(AcquisitionInfo.DEFAULT, false, true);
         Object object = cache.get(oid, new ReadOnlyAcquisitionPolicy(pinAcquisition));
         return null != object;
+    }
+
+    protected Dispatcher locateDispatcher() {
+        DispatcherRegistry dispatcherRegistry;
+        try {
+            dispatcherRegistry = dispatcherRegistryClass.newInstance();
+        } catch (Exception e) {
+            throw new WADIRuntimeException("Cannot instantiate DispatcherRegistry", e);
+        }
+        
+        try {
+            return dispatcherRegistry.getDispatcherByClusterName(clusterName);
+        } catch (IllegalStateException e) {
+            throw new WADIRuntimeException("Cannot find cluster [" + clusterName + "]", e);
+        }
+    }
+
+    protected CacheStackContext newCacheStackContext(Dispatcher dispatcher) throws Exception {
+        ServiceSpaceName name = buildServiceSpaceName();
+        
+        CacheStackContext stackContext = new CacheStackContext(name, dispatcher);
+        stackContext.setDisableReplication(true);
+        stackContext.build();
+        return stackContext;
+    }
+
+    protected ServiceSpaceName buildServiceSpaceName() {
+        // Add persistence unit name.
+        String cacheName = clusterName + "/" + getName();
+        return new ServiceSpaceName(URI.create(cacheName));
+    }
+    
+    protected GlobalObjectStore newGlobalObjectStore(CacheStackContext stackContext) {
+        ServiceSpace serviceSpace = stackContext.getServiceSpace();
+        Manager manager = stackContext.getManager();
+        
+        LocalPeer localPeer = serviceSpace.getLocalPeer();
+        AccessListener accessListener = new BasicAccessListener(localPeer);
+        Streamer streamer = new SimpleStreamer();
+        GlobalObjectStore globalObjectStore = new BasicGlobalObjectStore(accessListener, manager, streamer);
+        return globalObjectStore;
+    }
+    
+    protected Cache newCache(GlobalObjectStore globalObjectStore) throws Exception {
+        BasicInTxCacheFactory inTxCacheFactory = new BasicInTxCacheFactory(globalObjectStore);
+        Cache cache = new BasicCache(globalObjectStore, inTxCacheFactory);
+        cache = new TxDecoratorCache(cache);
+        return new TransactionManagerAwareCache(cache, conf.getManagedRuntimeInstance().getTransactionManager());
     }
 
 }
